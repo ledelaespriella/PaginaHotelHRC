bfb148c9b7a6939851cb52fc3ce6f4ee1e7426fd
luis Eduardo <luisdelaespriellaj@hotmail.com>
2021-10-11 18:17:01 -0500

    se actualiza la logica algoritmica y se agrega un homepage

--- main.py
+++ main.py
@@ -1,26 +1,83 @@
-from flask import Flask
+import os
+import utils
+from flask import Flask, request, flash
 from flask import render_template
-app=Flask(__name__)
+from flask.helpers import url_for
+from werkzeug.utils import redirect
+import yagmail
 
-@app.route('/')
+app=Flask(__name__)
+app.secret_key = os.urandom(24)
+
+#pendiente modificar todas las url locales en los templates de los static
+
+
+@app.route('/', methods=['GET'])
+def inicio():
+    return render_template('Home.html')
+
+
 @app.route('/login',methods=['GET','POST'])
 def login():
-    #si ya inicio sesion -> muestra las habitaciones
-    #sino -> muestra login
-    return render_template('login.html')
+    if request.method == 'POST':
+        user= request.form["correo"]
+        password= request.form["pass"]
+        
+        if user=="luisdelaespriellaj@hotmail.com" and password=="12345":
+            return redirect(url_for("pagina"))
+        else:
+            return render_template('login.html')
+    else:
+        return render_template('login.html')
+    
+    #validar condiciones del usuario, correo electronico valido
+    
+    
 
 @app.route('/registro',methods=['GET','POST'])
 def registro():
-    return render_template('registro.html')
+    
+    try:
+        if request.method == 'POST':
+            correo= request.form["email"]
+            passw=request.form["password"]
+            error=None
+        
+            if not utils.isEmailValid(correo):
+                    error="Correo invalido."
+                    flash(error)
+                    return render_template("registro.html")
+            
+            if not utils.isPasswordValid(passw):
+                    error="Contraseña invalida por favor registre una correcta."
+                    flash(error)
+                    return render_template("registro.html")
+            
+            yag=yagmail.SMTP("pruebasluismintic","Darkluise2")
+            yag.send(to=correo, subject="Activa tu cuenta", contents="Bienvenido, usa este link para activar tu cuenta")
+            flash("Revisa tu correo para activar tu cuenta")        
+            return redirect(url_for("login"))
+        else:
+            return render_template('registro.html')
+    except:
+        return render_template('registro.html')
+    #guardar en un diccionario los datos
+    
 
 @app.route('/login/recuperacion',methods=['GET','POST'])
 def recuperacion():
-    return render_template('recuperacion.html')
+    if request.method == 'POST':
+        correo=request.form['correo']
+    else:
+        return render_template('recuperacion.html')
 
 @app.route('/login/recuperacion/mensaje',methods=['GET','POST'])
-def mensaje_recuperacion():
+def mensaje():
     return render_template('mensaje.html')
 
+@app.route('/login/pagina')
+def pagina():
+    return "<h1>Pagina de prueba</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- static/Script/index.js
+++ static/Script/index.js
@@ -1,3 +1,21 @@
-function validarLogin() {
-    alert('prueba');
+function validarRecuperacion() {
+    
+    var email=document.formRecuperacion.correo;
+
+    var formatoCorreo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    if(!email.value.match(formatoCorreo)){
+        alert("Debes ingresar un correo electronico válido");
+        correo.focus();
+    }
+}
+
+function validarRegistro() {
+    
+    var email=document.formRegistro.email;
+
+    var formatoCorreo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    if(!email.value.match(formatoCorreo)){
+        alert("Debes ingresar un correo electronico válido");
+        correo.focus();
+    }
 }
